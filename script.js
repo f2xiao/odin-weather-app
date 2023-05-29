83453486df638301cf4331d35378fa717df0a3cb
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-05-29 11:08:30 -0400

    Edit script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const input = document.querySelector('.search-container>input');
 const button = document.querySelector('.search-container>button');
-console.log(input);
-console.log(button);
+// console.log(input);
+// console.log(button);
 
 button.addEventListener('click', () => { 
     if(input.value){
-        // console.log(input.value);
+        console.log(input.value);
     }
  })
+
