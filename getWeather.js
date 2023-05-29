eb3b181a1bb3b5644fb19d98da57d12bd3a52322
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-05-29 15:37:30 -0400

    Edit getWeather.js

--- getWeather.js
+++ getWeather.js
@@ -1,3 +1,16 @@
-export default function(){
-    console.log('hello');
+import fetchData from "./fetchData.js";
+export default async function(API_KEY, BASE_URL, cityName){
+    try{
+    // console.log('hello');
+    // console.log(API_KEY);
+    // console.log(BASE_URL);
+    const url = `${BASE_URL}key=${API_KEY}&q=${cityName}&aqi=no`
+    // console.log(url);
+    const weather = (await fetchData(url)).current.condition;
+
+    return weather;
+
+    }catch(error){
+        console.log(error)
+    }
 }
