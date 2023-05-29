c88a20aec9918c75ce1848770d5e7af46404d4a6
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-05-29 15:38:40 -0400

    Edit script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import config from "./config.js"
+
 import getWeather from "./getWeather.js";
 const input = document.querySelector('.search-container>input');
 const button = document.querySelector('.search-container>button');
@@ -5,14 +7,17 @@ const button = document.querySelector('.search-container>button');
 // console.log(button);
 
 // TODO: get user input for location
-button.addEventListener('click', () => { 
+button.addEventListener('click', async () => { 
     if(input.value){
         console.log(input.value);
+        const {API_KEY, BASE_URL} = config.WEATHER;
+        const data = await getWeather(API_KEY, BASE_URL, input.value);
+        console.log(data);
     }
  })
-
+console.log()
 // TODO: fetch weather data with weather API
-getWeather();
+
 // TODO: fetch gif data with gifpy API
 
 // TODO: loading component with gifpy API
